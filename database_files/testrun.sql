945d4d13f0f92f59b79e92588f827a13e8a2ac37
Tito40 <gilbertorosester@gmail.com>
2018-03-07 04:49:27 -0500

    Add files via upload

---
+++ database_files/testrun.sql
@@ -0,0 +1,13 @@
+﻿-- Database: "PubHub"
+
+-- DROP DATABASE "PubHub";
+
+CREATE DATABASE "PubHub"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE BOOK_TAGS (Perons int);
